bfcbcd51432afcda113a12b4e5375973de9c3b1b
Todd <toddolas+regret@gmail.com>
2021-12-14 21:51:04 -0800

    added profile

---
+++ xatDaddyV2/assets/js/profile.js
@@ -0,0 +1,16 @@
+
+$(function () {
+
+$('.right-menu a').click(function(){
+ $(this).siblings().removeClass('active');
+  $(this).addClass('active');
+
+
+var tab = $(this).attr('href').replace('#','.');
+$('.content>div').removeClass('open');
+$(tab).addClass('open');
+
+});
+
+
+});
